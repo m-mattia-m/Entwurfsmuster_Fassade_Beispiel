14da0b47f4273b59eb47d246184f3447e402bbd6
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2020-12-15 14:42:14 +0100

    v3 (with output)

--- src/Frontend.java
+++ src/Frontend.java
@@ -8,6 +8,12 @@ public class Frontend {
 		f.postUser("Max2", "Mustermann", 17);
 		f.postUser("Max3", "Mustermann", 17);
 		f.getUsers();
+		
+		/* Output:
+		* User@4517d9a3
+		* User@372f7a8d
+		* User@2f92e0f4
+		*/
 
 	}
 	
